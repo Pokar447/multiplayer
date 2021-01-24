1d9d22f9cec1ca69a672a91fa0eec8123b13525c
Jorn <jorn.ihlenfeldt@stud.th-luebeck.de>
2021-01-24 19:31:01 +0100

    Final SQL-Script, changed few printlns in Server

--- server/multiplayer.sql
+++ server/multiplayer.sql
@@ -3,26 +3,31 @@ CREATE SCHEMA `multiplayer`;
 USE `multiplayer`;
 
 CREATE TABLE `application_user` (
-                        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                        `username` VARCHAR(20) NOT NULL UNIQUE,
-                        `email` VARCHAR(100) NOT NULL UNIQUE,
-                        `password` VARCHAR(100) NOT NULL
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL UNIQUE,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(100) NOT NULL
 ) DEFAULT CHARSET=utf8;
 
 # Password == 'Password123!'
 INSERT INTO application_user (id, username, email, password) VALUES
 (1, 'Jorn', 'jorn.ihlenfeldt@stud.th-luebeck.de', '$2a$10$ADIbMfPC6jLkBrKVe/v1ceKMYn3FGh6Z/m6M2VHiLSjkuEsAT2gg.'),
-(2, 'Nora', 'nora.kuehnel@stud.th-luebeck.de', '$2a$10$bvvOPTPKsnVyeFAy1nN/yuPRq1w/atbzs.vhic2u.BWzUwhiZYBtG');
+(2, 'Nora', 'nora.kuehnel@stud.th-luebeck.de', '$2a$10$bvvOPTPKsnVyeFAy1nN/yuPRq1w/atbzs.vhic2u.BWzUwhiZYBtG'),
+(3, 'HerrEhlers', 'herr@ehlers.de', '$2a$10$WK7x0F9Z5LWwns3L2z7uq.XSsRmDZbdEDQciycbmm1ZkuGj0NR3cO');
 
 CREATE TABLE `history` (
-                                    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                                    `user_id` INT(20) NOT NULL,
-                                    `opponent_id` INT(20) NOT NULL,
-                                    `user_hp` INT(20) NOT NULL,
-                                    `opponent_hp` INT(20) NOT NULL,
-                                    `date_time` DATETIME NOT NULL
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT(20) NOT NULL,
+    `opponent_id` INT(20) NOT NULL,
+    `user_hp` INT(20) NOT NULL,
+    `opponent_hp` INT(20) NOT NULL,
+    `date_time` DATETIME NOT NULL
 ) DEFAULT CHARSET=utf8;
 
 INSERT INTO history (id, user_id, opponent_id, user_hp, opponent_hp, date_time) VALUES
 (1, 1, 2, 3, 0, now()),
-(2, 2, 1, 0, 3, now());
+(2, 2, 1, 0, 3, now()),
+(3, 1, 3, 0, 1, now()),
+(4, 3, 1, 1, 0, now()),
+(5, 2, 3, 3, 0, now()),
+(6, 3, 2, 0, 3, now());
