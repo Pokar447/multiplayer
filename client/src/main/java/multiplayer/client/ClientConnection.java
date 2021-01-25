b4bc6eb9eccee8921021e4ccee204e769f17a3a6
Jorn <jorn.ihlenfeldt@stud.th-luebeck.de>
2021-01-25 11:51:35 +0100

    ClientConnection comments

--- client/src/main/java/multiplayer/client/ClientConnection.java
+++ client/src/main/java/multiplayer/client/ClientConnection.java
@@ -5,6 +5,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * Client Connection to the server
+ *
+ * @author      Nora Kühnel <nora.kuhnel@stud.th-luebeck.de>
+ * @author      Jorn Ihlenfeldt <<jorn.ihlenfeldt@stud.th-luebeck.de>
+ *
+ * @version     1.0
+ */
 class ClientConnection {
 
     private Client client;
@@ -12,6 +20,9 @@ class ClientConnection {
     private DataInputStream dataInput;
     private DataOutputStream dataOutput;
 
+    /**
+     * Client Connection constructor
+     */
     public ClientConnection() {
         System.out.println("-----Client-----");
         try {
@@ -32,6 +43,11 @@ class ClientConnection {
         }
     }
 
+    /**
+     * Sends the button number that the user clicked to the other client
+     *
+     * @param number Button number that the user clicked
+     */
     public void sendButtonNum(int number) {
         try {
             dataOutput.writeInt(number);
@@ -41,6 +57,11 @@ class ClientConnection {
         }
     }
 
+    /**
+     * Receives the button number which the enemy clicked
+     *
+     * @return ButtonNumber Button number that the enemy clicked
+     */
     public int receiveButtonNum() {
         int n = -1;
         try {
@@ -51,46 +72,67 @@ class ClientConnection {
         return n;
     }
 
+    /**
+     * Sends the character id that the user chose to the other client
+     *
+     * @param characterId Character id that the user has chosen
+     */
     public void sendCharacter (int characterId) {
-        System.out.println("sendCharacter: " + characterId);
         try {
             dataOutput.writeInt(characterId);
             dataOutput.flush();
         } catch (IOException exception) {
-            System.out.println("IOException from sendButtonNum() ClientConnection");
+            System.out.println("IOException from sendCharacter() ClientConnection");
         }
     }
 
+    /**
+     * Sends the id of the user to the other client
+     *
+     * @param userId User id of the user
+     */
     public void sendUserId (int userId) {
-        System.out.println("sendUser: " + userId);
         try {
             dataOutput.writeInt(userId);
             dataOutput.flush();
         } catch (IOException exception) {
-            System.out.println("IOException from sendButtonNum() ClientConnection");
+            System.out.println("IOException from sendUserId() ClientConnection");
         }
     }
 
+    /**
+     * Receives the character id that the other client has chosen
+     *
+     * @return CharacterId Character id the enemy has chosen
+     */
     public int receiveCharacter() {
         int n = -1;
         try {
             n = dataInput.readInt();
         } catch (IOException exception) {
-            System.out.println("IOException from receiveButtonNum() ClientConnection");
+            System.out.println("IOException from receiveCharacter() ClientConnection");
         }
         return n;
     }
 
+    /**
+     * Receives the user id that the other client has chosen
+     *
+     * @return UserId User id the enemy has chosen
+     */
     public int receiveUserId() {
         int n = -1;
         try {
             n = dataInput.readInt();
         } catch (IOException exception) {
-            System.out.println("IOException from receiveButtonNum() ClientConnection");
+            System.out.println("IOException from receiveUserId() ClientConnection");
         }
         return n;
     }
 
+    /**
+     * Sends the ready state to the other client
+     */
     public void sendUserReady () {
         System.out.println("User is ready!");
 
@@ -102,6 +144,11 @@ class ClientConnection {
         }
     }
 
+    /**
+     * Receives the ready state from the other client
+     *
+     * @return ReadyState Information if the other client is ready
+     */
     public int receiveUserReady () {
         int n = -1;
         try {
