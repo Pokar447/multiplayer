fe9e88beb47ed4d95709868e61420c0344e5beeb
Jorn <jorn.ihlenfeldt@stud.th-luebeck.de>
2021-01-25 11:29:56 +0100

    Comment and clean up GameController + History (client)

--- client/src/main/java/multiplayer/client/History.java
+++ client/src/main/java/multiplayer/client/History.java
@@ -1,5 +1,13 @@
 package multiplayer.client;
 
+/**
+ * History class to create entries in the Statistics section
+ *
+ * @author      Nora Kühnel <nora.kuhnel@stud.th-luebeck.de>
+ * @author      Jorn Ihlenfeldt <<jorn.ihlenfeldt@stud.th-luebeck.de>
+ *
+ * @version     1.0
+ */
 public class History {
     public int id;
     public int userId;
@@ -7,34 +15,60 @@ public class History {
     public int userHp;
     public int opponentHp;
     public String date;
-    public String opponentname;
+    public String opponentName;
 
-    public History(int id, int userId, int opponentId, int userHp, int opponentHp, String date, String opponentname) {
+    /**
+     * History constructor
+     *
+     * @param id Unique id of the history
+     * @param userId User id the history belongs to
+     * @param opponentId User id of the opponent
+     * @param userHp Health points of the user
+     * @param opponentHp Health points of the opponent
+     * @param date Date and time when the game took place
+     * @param opponentName User name of the opponent
+     */
+    public History(int id, int userId, int opponentId, int userHp, int opponentHp, String date, String opponentName) {
         this.id = id;
         this.userId = userId;
         this.opponentId = opponentId;
         this.userHp = userHp;
         this.opponentHp = opponentHp;
         this.date = date;
-        this.opponentname = opponentname;
+        this.opponentName = opponentName;
     }
 
+    /**
+     * @return userHp Health Points of the user
+     */
     public int getUserHp() {
         return userHp;
     }
 
-    public String getOpponentname() {
-        return opponentname;
+    /**
+     * @return opponentName Name of the opponent
+     */
+    public String getOpponentName() {
+        return opponentName;
     }
 
+    /**
+     * @return opponentId User id of the opponent
+     */
     public int getOpponentId() {
         return opponentId;
     }
 
+    /**
+     * @return opponentHp Health points of the opponent
+     */
     public int getOpponentHp() {
         return opponentHp;
     }
 
+    /**
+     * @return date Date and time when the game took place
+     */
     public String getDate() {
         return date;
     }
